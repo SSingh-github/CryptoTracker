132bcbb7fc953a6a781fd0fba3688bd4a1ffb159
Sukhpreet Singh <ss0218308@gmail.com>
2026-01-19 17:01:41 +0530

    create coin row view and pass the coin model in it

---
+++ CryptoTracker/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,17 @@
+//
+//  CoinRowView.swift
+//  CryptoTracker
+//
+//  Created by Sukhpreet Singh on 19/01/26.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    let coin: CoinModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
