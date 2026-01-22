d0ede083b500e2ea6911cf26291db7bea2bcf9f5
Sukhpreet Singh <ss0218308@gmail.com>
2026-01-22 12:51:11 +0530

    create home view model and use it as environment object in the app

---
+++ CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewModel.swift
+//  CryptoTracker
+//
+//  Created by Sukhpreet Singh on 20/01/26.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class HomeViewModel: ObservableObject {
+    
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        }
+    }
+
+}
