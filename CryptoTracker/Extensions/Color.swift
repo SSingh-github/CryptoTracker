fd171c09a9947b52d0d37713b510eb6c0f6016ef
Sukhpreet Singh <ss0218308@gmail.com>
2026-02-11 14:37:13 +0530

    create color extension in launch screen

--- CryptoTracker/Extensions/Color.swift
+++ CryptoTracker/Extensions/Color.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 extension Color {
     static let theme = ColorTheme()
+    static let launch = LaunchTheme()
 }
 
 struct ColorTheme {
@@ -20,4 +21,9 @@ struct ColorTheme {
     let red = Color("RedColor")
     let secondaryText = Color("SecondaryTextColor")
     let shimmerBackground = Color("ShimmerColor")
+}
+
+struct LaunchTheme {
+    let background = Color("LaunchBackgroundColor")
+    let accent = Color("LaunchAccentColor")
 }
